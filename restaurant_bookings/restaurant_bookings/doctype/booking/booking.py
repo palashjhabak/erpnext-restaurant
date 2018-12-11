6bdf39a13f76bd5db9a078d13bdbaa4d03c7ea16
Palash Jhabak <palash@techlift.in>
2018-12-11 20:03:46 +0530

    Custom Field Set Booking Doctype

--- restaurant_bookings/restaurant_bookings/doctype/booking/booking.py
+++ restaurant_bookings/restaurant_bookings/doctype/booking/booking.py
@@ -8,7 +8,12 @@ from frappe.model.document import Document
 from frappe import _, throw
 
 class Booking(Document):
-	pass
+	def validate(self):
+		self.set_booking_title()
+
+	def set_booking_title(self):
+		self.custom_title = " ".join(filter(None, [self.booking_type, self.min_people, 'People']))
+
 
 def set_booking_event_day():
 	frappe.db.sql("""update tabBooking set event_day = 'Today' where date = CURDATE()""")
