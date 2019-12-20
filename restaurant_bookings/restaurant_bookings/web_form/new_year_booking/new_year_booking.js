e07c61fcb8068e46f4bdd67225adc2e4acd4379a
palashjhabak <palash@techlift.in>
2019-12-20 23:12:09 +0530

    changes

--- restaurant_bookings/restaurant_bookings/web_form/new_year_booking/new_year_booking.js
+++ restaurant_bookings/restaurant_bookings/web_form/new_year_booking/new_year_booking.js
@@ -98,15 +98,19 @@
 // })
 alert('ggggggg')
 
-frappe.web_form.on('name1', (field, value) => {
-    if (value < 1000) {
-        frappe.web_form.set_df_property('rate', 'hidden', 1);
-    }
-});
+frappe.web_form.after_load = () {
+    frappe.web_form.on('name1', (field, value) => {
+		if (value < 1000) {
+			frappe.web_form.set_df_property('rate', 'hidden', 1);
+		}
+	});
+	
+	
+	frappe.web_form.on('couple', (field, value) => {
+		if (value < 1000) {
+			frappe.web_form.set_df_property('rate', 'hidden', 1);
+		}
+	});
+}
 
 
-frappe.web_form.on('couple', (field, value) => {
-    if (value < 1000) {
-        frappe.web_form.set_df_property('rate', 'hidden', 1);
-    }
-});
